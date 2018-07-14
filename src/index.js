a1e513a444c956c865281d8efe484d5a1d5683b2
Jason Godson <jason.godson@shopify.com>
2018-07-14 06:35:32 -0600

    Add bugsnag

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import bugsnag from 'bugsnag-js';
+import createPlugin from 'bugsnag-react';
+import version from './version.json';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bugsnagClient = bugsnag({
+  apiKey: '733fe1744f51faceca41123ed92afa72',
+  appVersion: version.version,
+  notifyReleaseStages: ['production'],
+});
+const ErrorBoundary = bugsnagClient.use(createPlugin(React));
+bugsnagClient.notify(new Error('Test error'))
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
